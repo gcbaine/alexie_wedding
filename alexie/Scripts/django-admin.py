9dfabe409afdb51e6b955c2224a6b9f87d8dcc64
Gary C. Baine II <garybaine@gmail.com>
2020-07-13 19:26:42 -0400

    init commit

---
+++ alexie/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\garyb\desktop\oaken projects\alexiewedding\alexie\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
